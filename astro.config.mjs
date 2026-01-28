8399ab1c84c3df129c9d2f9cd5d4c4f8b544ba18
Timothy Li <himynameistimli@gmail.com>
2026-01-28 04:05:11 -0500

    Upgrade to Astro 5 with Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'hybrid',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   integrations: [tailwind()]
 });
